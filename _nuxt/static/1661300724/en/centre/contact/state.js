dfa7b6eeb64e469002f7bd93864794fa503b172f
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-08-24 05:25:53 +0500

    dacc first pro

---
+++ _nuxt/static/1661300724/en/centre/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661300724",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fcentre\u002Fcontact",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
